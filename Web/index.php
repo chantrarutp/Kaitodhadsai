9982d26890264662e1498859daf110594faf074c
chantrarutp <116155098+chantrarutp@users.noreply.github.com>
2025-02-05 22:19:07 +0700

    Create index.php

---
+++ Web/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body class="">
+  <div class="container">
+    <header>
+      <div class="title" style="color: #f3f3ed;">Kaitodhadsai</div>
+      <a href="indexen.php" class="tran">EN</a>
+      <div class="icon-cart">
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
+        </svg>
+        <span>0</span>
+      </div>
+    </header>
+    <div class="listProduct">
+    </div>
+  </div>
+  <div class="cartTab">
+    <h1>รายการที่เลือก</h1>
+    <div class="listCart">
+    </div>
+    <div class="totalPriceDisplay">
+      ไก่ทอด: $<span id="totalPriceDisplayValue"></span>
+      <p>เครื่องเคียง: $<span id="addonPriceDisplayValue"></span></p>
+
+    </div>
+    <div class="btn">
+      
+      <button class="close">ปิด</button>
+      <a href="websiteconfirm.html" class="checkOut">
+        ต่อไป
+      </a>
+    </div>
+  </div>
+
+  <script src="app.js"></script>
+  <br><br><br>
+  <footer>
+    <p>คำชี้แจงความเป็นส่วนตัว | เงื่อนไขการใช้บริการ</p>
+    <p>อุปกรณ์ขัดข้องติดต่อ : 000-000-0000</p>
+    <p>&copy; 2024 Fried Chicken Sandy Beach</p>
+  </footer>
+</body>
+</html>
